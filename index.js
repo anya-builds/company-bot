2ea5b0e252d4e501afac3765c01ea917b3a636ef
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-08 23:33:44 +0530

    company-bot

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("welcome to company chatbot!")
+/**
+ * Stage 1: indexing
+ * implementation plan
+ * 1. Load the document - pdf,text
+ * 2. Chunk the document
+ * 3. Generate vector embeddings
+ * 4. Store the vector embeddings - vector datatbase
+ * 
+ * stage 2:
+ * 1. setup LLM
+ * 2. Add retrieval step
+ * 3. Pass input+ relevant information to LLM
+
+ */

f723fcff5803a9a5443abf73036407796ebd5f51
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-09 17:51:39 +0530

    company-bot

--- chat.js
+++ chat.js
@@ -1,45 +1,45 @@
-// import readline from 'node:readline/promises';
-// import Groq from 'groq-sdk';
-// import { vectorStore } from './prepare.js';
+import readline from 'node:readline/promises';
+import Groq from 'groq-sdk';
+import { vectorStore } from './prepare.js';
 
-// const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-// export async function chat() {
-//     const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+export async function chat() {
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-//     while (true) {
-//         const question = await rl.question('You: ');
-//         if (question === '/bye') {
-//             break;
-//         }
+    while (true) {
+        const question = await rl.question('You: ');
+        if (question === '/bye') {
+            break;
+        }
 
-//         // retrieval
-//         const relevantChunks = await vectorStore.similaritySearch(question, 3);
-//         const context = relevantChunks.map((chunk) => chunk.pageContent).join('\n\n');
+        // retrieval
+        const relevantChunks = await vectorStore.similaritySearch(question, 3);
+        const context = relevantChunks.map((chunk) => chunk.pageContent).join('\n\n');
 
-//         const SYSTEM_PROMPT = `You are an assistant for question-answering tasks. Use the following relevant pieces of retrieved context to answer the question. If you don't know the answer, say I don't know.`;
+        const SYSTEM_PROMPT = `You are an assistant for question-answering tasks. Use the following relevant pieces of retrieved context to answer the question. If you don't know the answer, say I don't know.`;
 
-//         const userQuery = `Question: ${question}
-//         Relevant context: ${context}
-//         Answer:`;
-//         const completion = await groq.chat.completions.create({
-//             messages: [
-//                 {
-//                     role: 'system',
-//                     content: SYSTEM_PROMPT,
-//                 },
-//                 {
-//                     role: 'user',
-//                     content: userQuery,
-//                 },
-//             ],
-//             model: 'llama-3.3-70b-versatile',
-//         });
+        const userQuery = `Question: ${question}
+        Relevant context: ${context}
+        Answer:`;
+        const completion = await groq.chat.completions.create({
+            messages: [
+                {
+                    role: 'system',
+                    content: SYSTEM_PROMPT,
+                },
+                {
+                    role: 'user',
+                    content: userQuery,
+                },
+            ],
+            model: 'llama-3.3-70b-versatile',
+        });
 
-//         console.log(`Assistant: ${completion.choices[0].message.content}`);
-//     }
+        console.log(`Assistant: ${completion.choices[0].message.content}`);
+    }
 
-//     rl.close();
-// }
+    rl.close();
+}
 
-// chat();
+chat();
